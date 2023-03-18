6c15f9980fd992da14c839d77a09954443af74da
Daria <dariawisniewska575@gmail.com>
2023-03-18 08:37:08 +0100

    init login

--- web_project/urls.py
+++ web_project/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from AdSpot.views import advertisement, index
 # routing 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    path('ad/<id>/', advertisement, name='advertisement')
+    path('ad/<id>/', advertisement, name='advertisement'),
+    path('login/', auth_views.LoginView.as_view(), name='login')
 ]
